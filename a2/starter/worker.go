46d3255f5fd96f0bee1d5ddfb1135e0ee6a706af
gs0930 <suhgaeun0930@gmail.com>
2024-02-06 11:35:30 -0500

    Add starter files

---
+++ a2/starter/worker.go
@@ -0,0 +1,12 @@
+package main
+package mapreduce
+type word String
+// Question: is parsing the doc done outside a function?
+type word {
+	string word
+}
+func (t *word) map(args *word, )
+
+func main() {
+	// TODO: add argument processing and run both single-threaded and multi-threaded functions
+}
