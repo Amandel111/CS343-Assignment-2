f71e937c9ac474af429b320d95e7110d83174617
Anabelle Mandel <anabelle.world@yahoo.com>
2024-02-10 13:43:29 -0500

    split content into chunks

--- client/leader.go
+++ client/leader.go
@@ -5,121 +5,121 @@ import (
 	"fmt"
 	//"client"
 	"log"
-
+	"strings"
 	//"errors"
+	"os"
+	"strconv"
+	"bufio"
 
 	
 )
-// leader and worker are servers
-// the leader is supposed to call the worker's rpcs
-// a worker 
+
 type Leader struct {
-	files [] string // files
-	// address of worker computer?
+	files [] string 
 }
 
 
 type InputChunk struct{
-	//string chunkID
 	ChunkContent string
 }
 
 
 //maybe the leader has an RPC that map can call in order to notify  the leader where it has stored info
 
-// func rpcCall(chunk []string) {
-// 	//call map function
-// 	//FIX
-// 	// args := &server.Args{chunk}
-// 	args := &Args{chunk}
-// 	fmt.Printf("Reached");
-// 	var reply int
-// 	err = client.Call("WordCount.Map", args, &reply)
-// 	if err != nil {
-// 		log.Fatal("map error:", err)
-// 	}
-// 	fmt.Printf("Map: %d", reply)
-// 	}
-	// map := new(Map)
-	// divCall := client.Go("Arith.Divide", args, quotient, nil)
-	// replyCall := <-divCall.Done	// will be equal to divCall
-	
-func main() {
-	
-	// directory := os.Args[1]
-    // files, err := os.ReadDir(directory)
-    // if err != nil {
-    //     fmt.Println("Error:", err)
-    //     return
-    // }
-    // var fileNames []string
-    // for _, file := range files {
-    //     // fmt.Println(file.Name())
-    //     fileNames = append(fileNames, directory+"/"+file.Name())
-    // }
-
-	// var lines []string
-	// for _, fileName := range fileNames {
-	// 	file, err := os.Open(fileName)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer file.Close()
-
-	// 	fileScanner := bufio.NewScanner(file)
-
-	// 	fileScanner.Split(bufio.ScanLines)
-
-	// 	// scan each file line by line
-	// 	for fileScanner.Scan() {
-	// 		line := fileScanner.Text()
-	// 		lines = append(lines, line)
-	// 	}
-	// }
-	// numLinesPerChunk := len(lines) / numChunks
-
-
-    // for index := 0; index < numChunks; index++ {
-
-    //     lowerBound := index * numLinesPerChunk
-    //     upperBound := lowerBound + numLinesPerChunk
-    //     slice := lines[lowerBound:upperBound]
-    //     //wg.Add(1) // do we need this?
-    //     rpcCall(slice)
-    // }
-    // slice := lines[numLinesPerChunk*numChunks : len(lines)]
-    // //wg.Add(1)
-    // //go start_thread(slice, numChunks)
-    // wg.Wait()
-
-	//dial the server
-	serverAddress := "127.0.0.1"
-	client, err := rpc.DialHTTP("tcp", serverAddress + ":1235")
+func MapCall(content string, port int) map[string]int {
+	serverAddress := "127.0.0.1:"
+	client, err := rpc.DialHTTP("tcp", serverAddress + strconv.Itoa(port))
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
-	
-	// Calling Map
-	chunk := "hello"
-	
-	//rpcCall(array)
-	args := chunk //go's version of a constructor, passing the paramter to be saved in the object
+	//call map RPC
+	// args := content //go's version of a constructor, passing the paramter to be saved in the object
 	fmt.Printf("Reached");
 	var reply map[string]int
-	err = client.Call("WordCount.Map", args, &reply)
+	err = client.Call("WordCount.Map", content, &reply)
 	if err != nil {
 		log.Fatal("map error:", err)
 	}
 	fmt.Printf("Map: %d", reply)
 
-	// Calling Reduce
-	listOfMaps := make([]map[string]int, 1)
-	var response map[string]int
-	err = client.Call("WordCount.Reduce", listOfMaps, &response) // fix
-	if err != nil {
-		log.Fatal("reduce error:", err)
-	}
-	fmt.Printf("Reduce: %d", reply)
+	return reply
+}
+
+// func ReduceCall(mapList []map[string]int) map[string]int{
+// 	// Calling Reduce
+// 	//listOfMaps := make([]map[string]int, 1)
+// 	var response map[string]int
+// 	err = client.Call("WordCount.Reduce", mapList, &response) // fix
+// 	if err != nil {
+// 		log.Fatal("reduce error:", err)
+// 	}
+// 	fmt.Printf("Reduce: %d", reply)
+
+// 	return response
 	
+// }
+	
+func main() {
+
+	//dial the server
+	
+
+	
+	directory := os.Args[1]
+    files, err := os.ReadDir(directory)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    var fileNames []string
+    for _, file := range files {
+        // fmt.Println(file.Name())
+        fileNames = append(fileNames, directory+"/"+file.Name())
+    }
+
+	var lines []string
+	for _, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		fileScanner := bufio.NewScanner(file)
+
+		fileScanner.Split(bufio.ScanLines)
+
+		// scan each file line by line
+		for fileScanner.Scan() {
+			line := fileScanner.Text()
+			lines = append(lines, line)
+		}
+	}
+	var numChunks int = 1 //this should always be equal to the number of workers running - 1 if numChunks > 1
+
+	numLinesPerChunk := len(lines) / numChunks
+	listOfMaps := make([]map[string]int, numChunks+1)
+
+	portNumber := 1233
+    for index := 0; index < numChunks; index++ {
+
+        lowerBound := index * numLinesPerChunk
+        upperBound := lowerBound + numLinesPerChunk
+        slice := lines[lowerBound:upperBound]
+		//loop thorugh slice, append each element to contentAsString
+		contentAsString := strings.Join(slice, " ")
+        //wg.Add(1) // do we need this?
+		portNumber += 1 
+        listOfMaps = append(listOfMaps, MapCall(contentAsString ,portNumber))
+    }
+    slice := lines[numLinesPerChunk*numChunks : len(lines)]
+	contentAsString := strings.Join(slice, " ")
+
+	listOfMaps = append(listOfMaps, MapCall(contentAsString ,portNumber))
+	fmt.Print("list of maps", listOfMaps)
+
+    //wg.Add(1)
+    //go start_thread(slice, numChunks)
+    //wg.Wait
 
 }
--- server/worker.go
+++ server/worker.go
@@ -40,34 +40,16 @@ type InputChunk struct{
 func (t *WordCount) Map(content string, reply *map[string]int) error {
 	//for 
 	fmt.Println("Hello world")
-	fmt.Println("Mapper", content)
+	fmt.Println("Mapper", content[:20])
 	fmt.Println(reply)
 	dict := make(map[string]int)
-	*reply = dict //this is going to be key value, pair of a word and its count 1
-	//this reply is going to be the location of the key value pair, and this location gets sent to reduce
 	
-	//should the leader be accessing intermediate info via reply from RPC
-	//or should leader have an rpc that gets called by mapper periodically int order to
-	//pass location info?
-
-	//what does leader pass to reduce? Does it just pass the lcoation of the info stored on mapper pc
-	//or also pass the key value pair of word type and count?
-
-	//how does the mapper store its data locally? How do we code this? Is it all stored in a file
-	//or some other contiguous chunk? 
-
-	// who/what reads the files themselves - prolly the leader?
+	dict["hello"] = 1
+	*reply = dict //this is going to be key value, pair of a word and its count 1
+	
 	return nil
 }
-/*
-Step 1: leader calls RPC map and passes InputChunk w file info to parse
-Step 2: map parces InputChunk into key-value pairs and stores those locally
-Step 3: Once map has parced all of InputChunk, it "replies" to the leader
-with the location address of the information it stored
-Step 4: Leader calls RPC reduce and passes location information from mapper 
-to reducer so that the reducer 
-Find all the values of the word through reduce and then a new reduce process ocucrs
-*/
+
 // reducer looks at what information is stored in files and returns to leader?
 func (t *WordCount) Reduce(args *[]map[string]int, reply *map[string]int) error {
 	//originally had args *Maps
@@ -80,12 +62,6 @@ func (t *WordCount) Reduce(args *[]map[string]int, reply *map[string]int) error 
 	return nil
 }
 
-// leader reads in; make a struct of a map of values and store it there
-// main function in worker depends, leader should have a main function
-// worker is the processt that does the work
-
-// when the reduction is done, outputs to local files and then files are combined
-// and stored in the DFS
 
 	
 func main() {
@@ -93,7 +69,7 @@ func main() {
 	words := new(WordCount)
 	rpc.Register(words)
 	rpc.HandleHTTP()
-	l, err := net.Listen("tcp", "127.0.0.1:1235")
+	l, err := net.Listen("tcp", "127.0.0.1:1234")
 	if err != nil {
 		log.Fatal("listen error:", err)
 	}
